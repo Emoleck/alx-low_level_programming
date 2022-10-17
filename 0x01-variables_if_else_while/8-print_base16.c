d757ce68897351bafaf2c5af42fafaf4c5235e31
Emoleck <chigold89@gmail.com>
2022-10-17 15:46:32 -0700

    modify all

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - program entry point.
+*Return:0 if no errors, non zero if no errors.
+*/
+int main(void)
+{
+int i;
+char hexvalues[] =
+"0123456789abcdef";
+
+for (i = 0; i < 16; i++)
+{
+putchar(hexvalues[i]);
+}
+putchar ('\n');
+return (0)
+}
